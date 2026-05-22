a7c06a7ba1124a15afb8c3fb7f7111d70e8aa4fb
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2026-05-22 12:11:00 -0300

    alteração na pasta de routa do usuário

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { listUsers, createUser } from '../controllers/userController.js';
+import { createUser, listUsers } from '../controllers/userController.js';
 
 const router = express.Router();
 
