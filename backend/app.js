c6dc2c4c9c54a77d63753c7aa3282ed1e3518a1e
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2026-05-22 12:10:38 -0300

    ajuste de estrutura / criação da pasta de controllers

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import routes from './src/routes/index.js';
 import { initDatabase } from './src/database/index.js';
+import routes from './src/routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,6 +1,6 @@
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
 import path from 'path';
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
